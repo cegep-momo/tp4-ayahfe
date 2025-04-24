9764310be79efb663fac72727750f6b7ee3f784d
aya-hfe <imaya48484@gmail.com>
2025-04-24 15:12:43 -0400

    fin classe main

---
+++ main.py
@@ -0,0 +1,5 @@
+from controleur.controleur import Controleur
+
+if __name__ == "__main__":
+    controleur = Controleur()
+    controleur.demarrer()
--- mesure.py
+++
@@ -1,3 +0,0 @@
-class Mesure:
-    def __init__(self):
-        pass
