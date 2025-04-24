404522d9b0ac19aa618ff6a8c10dcec7c4212009
aya-hfe <imaya48484@gmail.com>
2025-04-24 15:02:20 -0400

    debut de la classe platine

---
+++ modele/platine.py
@@ -0,0 +1,7 @@
+from gpiozero import Button, DistanceSensor
+
+class Platine:
+    def __init__(self, pin_trigger=17, pin_echo=12, pin_start=26, pin_mesure=21):
+        self.capteur = DistanceSensor(trigger=pin_trigger, echo=pin_echo, max_distance=2)
+        self.start = Button(pin_start, pull_up=True)
+        self.btnMesure = Button(pin_mesure, pull_up=True)
