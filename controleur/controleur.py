266fb522dc5bbc861490d217b8f12f3444376ab3
aya-hfe <imaya48484@gmail.com>
2025-04-24 14:58:02 -0400

    sauvegarde pour les dates et les mesures dans un fichier json

--- controleur/controleur.py
+++ controleur/controleur.py
@@ -24,4 +24,33 @@ class Controleur:
             time.sleep(2)
             self.vue.effacer()
 
+    def prendre_mesure(self):
+        if not self.systeme_actif:
+            return
+        valeur = self.platine.lire_distance()
+        if valeur == -1.0:
+            self.vue.afficher("Erreur capteur", "Pas de retour")
+            return
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        mesure = Mesure(date, f"{valeur} cm")
+        self.vue.afficher("Mesure prise", f"{valeur} cm")
+        print(f"[MESURE] {valeur} cm")
+        self.sauvegarder(mesure)
+
+    def sauvegarder(self, mesure):
+        dossier = "sauvegarde"
+        fichier = os.path.join(dossier, "mesures.json")
+        if not os.path.exists(dossier):
+            os.makedirs(dossier)
+        if not os.path.exists(fichier):
+            with open(fichier, "w") as f:
+                pass  
+
+
+        with open(fichier, "a") as f:
+            json.dump(mesure.__dict__, f)
+            f.write("\n")
+
     
+
+   
